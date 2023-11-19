585d729e5ac221fe0a77ca9885c0f8c9c3fd1e60
Kevin Wang <kwang30@stuy.edu>
2023-11-18 22:54:05 -0500

    move stuffz

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,29 +1,81 @@
-'use client'
-import { useChat } from 'ai/react';
+//Home page which allows searching for a product
 
-export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { useRouter } from 'next/navigation'
+import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react"
+
+const formSchema = z.object({
+  query: z.string().min(2, {
+    message: "Type at least 2 characters.",
+  }),
+});
+
+export default function HomePage() {
+  const router = useRouter()
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: any) => {
+    router.push(`/search?q=${values.query}`)
+  };
+  // center the div on the page
   return (
-    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-      {messages.map(m => (
-        <div key={m.id}>
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+    // Flex container for centering
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <div className="mb-5">
+          <div className="flex justify-center">
+            <Image
+              src="/savvylogo.png"
+              alt="Savvy logo"
+              width={300}
+              height={300}
+            />
+          </div>
+          <p className="text-center text-gray-500">
+            Finds the best products for you
+          </p>
         </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Say something...
-          <input
-            className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
-            value={input}
-            onChange={handleInputChange}
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="query"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex space-x-2">
+                    <FormControl>
+                      <Input className="w-[500px]" placeholder="Start a shopping trip" {...field} />
+                    </FormControl>
+                    <Button type="submit">
+                      <Search className="w-4 h-4" />
+                    </Button>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <div className="h-[100px]">
+        </div>
+      </div>
     </div>
   );
 }
