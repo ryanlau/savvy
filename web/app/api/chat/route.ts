aef665aa0199eb3d84fb880ca326b630d6dcb550
Kevin Wang <kwang30@stuy.edu>
2023-11-19 06:49:46 -0500

    fix chat

--- web/app/api/chat/route.ts
+++ web/app/api/chat/route.ts
@@ -13,6 +13,7 @@ export const runtime = 'edge';
  * message history directly into the model.
  */
 const formatMessage = (message: VercelChatMessage) => {
+  if (message.role == "assistant") return ""
   return `${message.role}: ${message.content}`;
 };
 
@@ -56,14 +57,16 @@ export async function POST(req: NextRequest) {
   walmartProducts.forEach((product: any) => {
     initialPrompt += `${product.name}\n${product.price}\n${product.rating} stars\n${product.description}\n${product.img}\n${product.id}\n\n`
   })
+  
 
   initialPrompt += `\n\nReturn an array containing JSON objects. For each one of the products given above, return a JSON object that has the fields including the name, rating, price, explanation, img, id, and features. The name is a string, and is provided for each example. Acceptable values for rating is a number on a scale of 0 to 100, according to how well it fits the user's query. Take into account how many stars others have given it (given on a 0 to 5 scale) into account when computing your rating as well. The explanation is a string explaining the rating you gave, in detail, about what about the product warrants that rating for the user. If you deducted points, what about the product caused you to deducted points. If it has a particularly high score, why? Write about the features that set each product apart in the explanation. The feature field is a string of semicolon delimited features. Remember to do this for all products. Be sure to respond in English and ensure that the response is a complete and valid JSON object.\nCurrent conversation: {chat_history}\n\nUser: {input}\nAI:`
 
   const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-
+  
   const currentMessageContent = messages[messages.length - 1].content;
 
   const prompt = PromptTemplate.fromTemplate(initialPrompt);
+  console.log(prompt)
 
   const model = new ChatGoogleVertexAI({
     // maxOutputTokens: 10000,
